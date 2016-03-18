23f56fbdc97057d63affb2dc1a33d5f4610c5d8c
gasolin <gasolin@gmail.com>
2016-03-18 23:04:51 +0800

    lint fix

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-var Adapter = require("webbybot/src/adapter");
+import Adapter from 'webbybot/src/adapter';
 
 class MockAdapter extends Adapter {
-    send(envelope, ...strings) {
-      this.emit('send', envelope, strings);
-    }
+  send(envelope, ...strings) {
+    this.emit('send', envelope, strings);
+  }
 
-    emote(envelope, ...strings) {
-      this.emit('emote', envelope, strings);
-    }
+  emote(envelope, ...strings) {
+    this.emit('emote', envelope, strings);
+  }
 
-    reply(envelope, ...strings) {
-      this.emit('reply', envelope, strings);
-    }
+  reply(envelope, ...strings) {
+    this.emit('reply', envelope, strings);
+  }
 
-    topic(envelope, ...strings) {
-      this.emit('topic', envelope, strings);
-    }
+  topic(envelope, ...strings) {
+    this.emit('topic', envelope, strings);
+  }
 
-    play(envelope, ...strings) {
-      this.emit('play', envelope, strings);
-    }
+  play(envelope, ...strings) {
+    this.emit('play', envelope, strings);
+  }
 
-    run() {
-      this.emit("connected");
-    };
+  run() {
+    this.emit('connected');
+  };
 
-    close() {
-      this.emit("closed");
-    };
+  close() {
+    this.emit('closed');
+  };
 
-    receive(message) {
-      this.emit("received");
-    }
+  receive(message) {
+    this.emit('received');
+  }
 }
 
 export function use(robot) {
